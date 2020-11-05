501b61f4788ca2345dc6888fbce9cb2542b11a39
unknown <jeffersonsantos.a@gmail.com>
2020-11-05 19:30:19 -0300

    First commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,29 +1,56 @@
-import Transaction from '../models/Transaction';
+import Transaction from '../models/Transaction'
+
+interface CreateTransactionDTO {
+  title: string,
+  value: number,
+  type: 'income' | 'outcome'
+}
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+  income: number
+  outcome: number
+  total: number
 }
 
 class TransactionsRepository {
-  private transactions: Transaction[];
+  private transactions: Transaction[]
 
   constructor() {
-    this.transactions = [];
+    this.transactions = []
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
-  public getBalance(): Balance {
-    // TODO
+  public getBalance(transactions: CreateTransactionDTO[]): Balance {
+
+    const totalBalance = {
+      income: 0,
+      outcome: 0,
+      total: 0
+    }
+
+    transactions.map(function (obj) {
+      if (obj.type === "income") {
+        totalBalance.income = totalBalance.income + obj.value
+      } else {
+        totalBalance.outcome = totalBalance.outcome + obj.value
+      }
+    })
+
+    totalBalance.total = totalBalance.income - totalBalance.outcome
+
+    return totalBalance
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type })
+
+    this.transactions.push(transaction)
+
+    return transaction
   }
 }
 
-export default TransactionsRepository;
+export default TransactionsRepository
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,34 @@
-import TransactionsRepository from '../repositories/TransactionsRepository';
-import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository'
+import Transaction from '../models/Transaction'
+
+interface TransactionDTO {
+  title: string,
+  value: number,
+  type: 'income' | 'outcome'
+}
 
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
+  private transactionsRepository: TransactionsRepository
 
   constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
+    this.transactionsRepository = transactionsRepository
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: TransactionDTO): Transaction {
+    const transactions = this.transactionsRepository.all()
+    const balance = this.transactionsRepository.getBalance(transactions)
+    console.log(balance)
+
+    if (type === 'outcome' && value > balance.total) {
+      throw new Error("saldo indisponivel")
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type
+    })
+    return transaction
   }
 }
 
-export default CreateTransactionService;
+export default CreateTransactionService
